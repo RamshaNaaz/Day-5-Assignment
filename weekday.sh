cd9addc1447071a83bfc152e1f0c956126120252
RamshaNaaz <ramshanaaz93@gmail.com>
2022-05-12 19:40:07 +0530

    4 file to be commited

---
+++ weekday.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+echo "Enter the no between 1 to 7:"
+read n
+if [ $n -eq 1 ]
+then echo "Monday"
+
+  elif [ $n -eq 2 ]
+  then echo "Tuesday"
+
+    elif [ $n -eq 3 ]
+    then echo "Wednesday"
+        
+          elif [ $n -eq 4 ]
+            then echo "Thursday"
+ 
+              elif [ $n -eq 5 ]
+                then echo "Friday"
+              
+                    elif [ $n -eq 6 ]
+                      then echo "saturday"
+                    
+                         elif [ $n -eq 7]
+                            then echo "sunday"
+         else
+         echo "No out of range "
+fi
