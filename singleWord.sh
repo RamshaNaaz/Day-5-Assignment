a8bc0fbc9ac64f883ac9660a5f6945e3b218ae51
RamshaNaaz <ramshanaaz93@gmail.com>
2022-05-12 19:38:34 +0530

    3 file to be commited

---
+++ singleWord.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -x
+echo "Enter single digit no."
+read n
+
+if [ $n -eq 1 ]
+then
+echo "One"
+
+  elif [ $n -eq 2 ]
+  then
+     echo "Two"
+
+   elif [ $n -eq 3]
+    then  
+    echo "Three"
+
+       elif [ $n -eq 4 ]
+          then echo "Four"
+             
+              elif [ $n -eq 5 ]
+                 then echo "Five"
+  
+                      elif [ $n -eq 6 ]
+                       then echo "six" 
+                       
+                        elif [ $n -eq 7 ]
+                         then echo "seven"
+
+                             elif [ $n -eq 8 ]
+                               then echo "eight"
+                                 
+                                    elif [ $n -eq 9 ]
+                                      then echo "Nine"
+
+else
+echo "Enter single digit number for checking"
+
+fi
